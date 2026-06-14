52b0bd44bab55270d4a0f525e54d3a3e6418fa6d
xfgabx <fcajas193@gmail.com>
2026-06-14 12:44:39 -0500

    Update Ejemplo CRUD con LINQ.cs

--- CodigosEjemplos/Ejemplo CRUD con LINQ.cs
+++ CodigosEjemplos/Ejemplo CRUD con LINQ.cs
@@ -1,1 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
+class Producto
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public double Precio { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Producto> productos = new List<Producto>();
+
+        // CREATE
+        productos.Add(new Producto
+        {
+            Id = 1,
+            Nombre = "Mouse",
+            Precio = 15
+        });
+
+        // READ
+        var producto =
+            productos.FirstOrDefault(p => p.Id == 1);
+
+        Console.WriteLine(producto.Nombre);
+
+        // UPDATE
+        producto.Precio = 20;
+
+        // DELETE
+        productos.Remove(producto);
+
+        Console.WriteLine("Registros restantes: "
+                          + productos.Count);
+    }
+}
