58f721c233f4da91bebdf0d11fe7927b654059c9
xfgabx <fcajas193@gmail.com>
2026-06-14 12:43:27 -0500

    Update Ejemplo de Operadores en LINQ.cs

--- CodigosEjemplos/Ejemplo de Operadores en LINQ.cs
+++ CodigosEjemplos/Ejemplo de Operadores en LINQ.cs
@@ -1,1 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
+class Program
+{
+    static void Main()
+    {
+        List<int> ventas = new List<int>()
+        {
+            100,
+            250,
+            300,
+            150,
+            400
+        };
+
+        Console.WriteLine("Cantidad: " + ventas.Count());
+        Console.WriteLine("Suma: " + ventas.Sum());
+        Console.WriteLine("Promedio: " + ventas.Average());
+        Console.WriteLine("Máximo: " + ventas.Max());
+        Console.WriteLine("Mínimo: " + ventas.Min());
+    }
+}
